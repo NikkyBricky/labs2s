28673b0474735e7ce4fe87b7e8a0717c6cfe8b09
Nick <nick@MacBook-Air-Nick-2.local>
2026-05-11 14:35:44 +0300

    enhanced cycle output

--- flist_cycles/main.cpp
+++ flist_cycles/main.cpp
@@ -2,15 +2,35 @@
 #include <utility>
 #include "flist.h"
 
+void print_pair(std::pair<ForwardList::Node*, size_t> p){
+    if (p.first){
+        std::cout << "Cycle found: starts at " << p.second << " index" << std::endl;
+    }
+
+    else{
+        std::cout << "Cycle not found" << std::endl;
+    }
+}
+
 
 int main(){
-    ForwardList lst = ForwardList();
-    lst.push_back(1);
-    lst.push_back(2);
-    lst.push_back(3);
-    lst.push_back(4);
+    ForwardList lst1 = ForwardList();
+    lst1.push_back(1);
+    lst1.push_back(2);
+    lst1.push_back(3);
+    lst1.push_back(4);
 
-    lst.make_cycle(2);
-    std::pair<ForwardList::Node*, size_t> cycle_pair = lst.find_cycle();
-    std::cout << cycle_pair.second << std::endl;
+    lst1.make_cycle(2);
+    
+    ForwardList lst2 = ForwardList();
+    lst2.push_back(1);
+    lst2.push_back(2);
+    lst2.push_back(3);
+    lst2.push_back(4);
+
+    std::pair<ForwardList::Node*, size_t> cycle_pair = lst1.find_cycle();
+    std::pair<ForwardList::Node*, size_t> non_cycle_pair = lst2.find_cycle();
+
+    print_pair(cycle_pair);
+    print_pair(non_cycle_pair);
 }
